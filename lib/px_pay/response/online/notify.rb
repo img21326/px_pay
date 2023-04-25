9be76e58c4f306b7c1656fe75615e548dd8dbb65
AndrewLiao <andrew@weibyapps.com>
2023-04-25 12:42:21 +0800

    change order_id -> mer_trade_no

--- lib/px_pay/response/online/notify.rb
+++ lib/px_pay/response/online/notify.rb
@@ -8,15 +8,11 @@ module PxPay
           @raw.headers['PX-SignValue']
         end
 
-        def order_id
-          @mer_trade_no
-        end
+        attr_reader :mer_trade_no, :px_trade_no, :amount, :trade_amount, :discount_amount, :invo_carrier
 
         def bank_transaction_id
           @transaction_id
         end
-
-        attr_reader :px_trade_no, :amount, :trade_amount, :discount_amount, :invo_carrier
 
         def trade_time
           @trade_time = Time.parse(@trade_time) if @trade_time.instance_of? String
