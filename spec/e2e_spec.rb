47cd428423d36f30ce7a94f1fc946ed664084053
AndrewLiao <andrew@weibyapps.com>
2023-07-11 14:32:56 +0800

    add e2e test for payment

--- spec/e2e_spec.rb
+++ spec/e2e_spec.rb
@@ -15,46 +15,48 @@ RSpec.describe PxPay do
     config.secret_key = ENV['POS_SECRET_KEY']
     config
   end
-  let(:ec_order_id) { 1 }
+  let(:ec_order_id) { 'test_1231' }
   let(:pos_order_id) { 222_223_333 }
 
   it 'online_pay' do
-    request = PxPay::Request::Online::Pay.new(
-      mer_trade_no: ec_order_id,
-      amount: 100,
-      device_type: :pc,
-      web_confirm_url: 'http://example.com/confirm',
-      web_cancel_url: 'http://example.com/cancel'
-    )
+    request = PxPay::Request::Online::Pay.new({
+                                                order_id: ec_order_id,
+                                                amount: 1,
+                                                device_type: :pc,
+                                                web_confirm_url: 'http://example.com/confirm',
+                                                web_cancel_url: 'http://example.com/cancel',
+                                                notify_url: 'https://istore.weibyapps.com:8123/px_pay/result',
+                                                status_url: 'https://istore.weibyapps.com:8123/px_pay/orders'
+                                              })
     request.config = ec_config
     res = request.request
     expect(res.success?).to be(true)
   end
 
-  it 'pos_pay' do
-    request = PxPay::Request::Pos::Pay.new({
-                                             store_id: 1,
-                                             store_name: 'weiby_test',
-                                             pos_id: 1,
-                                             trade_time: Time.now,
-                                             merchant_trade_number: pos_order_id,
-                                             amount: 100,
-                                             pay_token: 'P2A27194D27P53GR1I'
-                                           })
+  # it 'pos_pay' do
+  #   request = PxPay::Request::Pos::Pay.new({
+  #                                            store_id: 1,
+  #                                            store_name: 'weiby_test',
+  #                                            pos_id: 1,
+  #                                            trade_time: Time.now,
+  #                                            merchant_trade_number: pos_order_id,
+  #                                            amount: 100,
+  #                                            pay_token: 'P2A27194D27P53GR1I'
+  #                                          })
 
-    request.config = pos_config
-    res = request.request
-    expect(res.success?).to be(true)
-  end
+  #   request.config = pos_config
+  #   res = request.request
+  #   expect(res.success?).to be(true)
+  # end
 
-  it 'pos_query' do
-    request = PxPay::Request::Pos::Query.new({
-                                               trade_number: '1#1'
-                                             })
+  # it 'pos_query' do
+  #   request = PxPay::Request::Pos::Query.new({
+  #                                              trade_number: '1#1'
+  #                                            })
 
-    request.config = pos_config
-    res = request.request
-    p request.send :end_point
-    expect(res.success?).to be(true)
-  end
+  #   request.config = pos_config
+  #   res = request.request
+  #   p request.send :end_point
+  #   expect(res.success?).to be(true)
+  # end
 end
